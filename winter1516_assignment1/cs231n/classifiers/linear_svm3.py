75e5bfcaca924b1946ed1d40a665692a51c46890
Prasasto Adi <prasastoadi@gmail.com>
2017-02-23 14:25:10 +0700

    Add svm loss naive

--- winter1516_assignment1/cs231n/classifiers/linear_svm3.py
+++ winter1516_assignment1/cs231n/classifiers/linear_svm3.py
@@ -18,22 +18,24 @@ def svm_loss_naive(W, X, y, reg):
   Returns a tuple of:
   - loss as single float
   - gradient with respect to weights W; an array of same shape as W
-  """
+ P """
   dW = np.zeros(W.shape) # initialize the gradient as zero
 
   # compute the loss and the gradient
   num_classes = W.shape[1]
   num_train = X.shape[0]
   loss = 0.0
-  for i in xrange(num_train):
+  for i in range(num_train):
     scores = X[i].dot(W)
     correct_class_score = scores[y[i]]
-    for j in xrange(num_classes):
+    for j in range(num_classes):
       if j == y[i]:
         continue
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dW[:,j] = dW[:,j] + X[i].T
+        dW[:,y[i]] = dW[:,y[i]] - X[i].T
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
@@ -51,6 +53,7 @@ def svm_loss_naive(W, X, y, reg):
   # code above to compute the gradient.                                       #
   #############################################################################
 
+  dW = dW/num_train + reg*W
 
   return loss, dW
 
