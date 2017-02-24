f3c0ed17cfe67bc688e3da9d1e84d7b790da5329
Prasasto Adi <prasastoadi@gmail.com>
2017-02-24 23:07:10 +0700

    Add LinearSVM with SGD

--- winter1516_assignment1/cs231n/classifiers/__init__.py
+++ winter1516_assignment1/cs231n/classifiers/__init__.py
@@ -1,3 +1,2 @@
-#from cs231n.classifiers.k_nearest_neighbor import *
 from cs231n.classifiers.k_nearest_neighbor3 import *
-#from cs231n.classifiers.linear_classifier import *
+from cs231n.classifiers.linear_classifier3 import *
